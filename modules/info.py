a8a9c98f6bc2b4d1d3dbb5c895017e33def01485
sbdipun <87560288+sbdipun@users.noreply.github.com>
2024-04-13 18:51:35 +0530

    Update info.py

--- modules/info.py
+++ modules/info.py
@@ -1,1 +1,9 @@
 
+from telethon import events
+
+@events.register(events.NewMessage(pattern='/info'))
+async def info_handler(event):
+    user = await event.client.get_entity(event.sender_id)
+    user_info_string = f"User ID: {user.id}\nFirst Name: {user.first_name}\nUsername: @{user.username}\nUser Link: [User Link](tg://user?id={user.id})"
+    await event.reply(user_info_string, parse_mode='md')
+  
