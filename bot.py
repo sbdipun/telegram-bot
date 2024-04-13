a6f65ec87731a08a56e23d116cf0efbc037364c1
sbdipun <87560288+sbdipun@users.noreply.github.com>
2024-04-13 23:15:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
+import asyncio
 import os
 import requests
+import random
 from telethon import TelegramClient, events
+from telethon.tl.types import InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv
 
 # Load environment variables
@@ -25,9 +28,41 @@ async def help_handler(event):
 * /help - You're already here!
 * /inspire - Receive a dose of motivation.
 * /joke - Tell me a funny joke. 
-* /about - Learn a bit more about me.  
+* /about - Learn a bit more about me.
     """
     await event.respond(help_message)
+
+
+@client.on(events.NewMessage(pattern='/joke'))
+async def joke_handler(event):
+    response = requests.get("https://official-joke-api.appspot.com/random_joke")
+    if response.status_code == 200:
+        joke_data = response.json()
+        if 'setup' in joke_data:  # Two-part joke
+            await event.respond(joke_data['setup'])
+            await asyncio.sleep(2)  # Add a slight delay for dramatic effect
+            await event.respond(joke_data['punchline'])
+        else:  # Single-line joke
+            await event.respond(joke_data['joke'])
+    else:
+        await event.respond("Oops, I couldn't find a funny joke right now. Try again later.")
+
+
+@client.on(events.NewMessage(pattern='/about'))
+async def about_handler(event):
+    markup = InlineKeyboardMarkup(
+        [
+            [  # First row
+                InlineKeyboardButton("My Dev", url="https://t.me/kinsboo7")
+            ],
+            [  # Second row
+                InlineKeyboardButton("Owner", url="https://t.me/slo_dev")
+            ]
+        ]
+    )
+
+    await event.respond("I'm a friendly Telegram bot still learning new tricks. I'm powered by Python", reply_markup=markup) 
+
 
 @client.on(events.NewMessage(pattern='/inspire'))
 async def inspire_handler(event):
@@ -39,7 +74,6 @@ async def inspire_handler(event):
     else:
         await event.respond("Oops, I couldn't fetch a quote right now. Try again later.")
 
-# ... Add more event handlers for /joke, /about, etc.
 
 def main():
     """Start the bot."""
