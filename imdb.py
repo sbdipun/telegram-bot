e3a0c3248ad801c46741e05d4ca8c5941417e1b8
sbdipun <87560288+sbdipun@users.noreply.github.com>
2024-04-25 10:15:01 +0530

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -9,7 +9,7 @@ from pyrogram.filters import command, regex
 from pyrogram.errors import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 import os
 import tgcrypto
-impost asynco
+import asynco
 from dotenv import load_dotenv
 
 
